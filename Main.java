26f41aa2507bb935cfd37dca074b464a18dee2ea
AVR21 <alejandroviera12@gmail.com>
2024-01-20 16:25:55 +0000

    First implementation.

--- Main.java
+++ Main.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 public class Main {
+    public static void main(String[] args) {
+        List<StudentData> students = CSVStudentDataLoader.create("datos.csv").load();
+        List<Double> data = new ArrayList<>();
+        students.forEach(s -> data.add(s.getNumberCourses()));
+        data.sort((i,j) -> {return (int)(i-j);});
+        TreeMap<Double, Integer> courses = CSVStudentDataLoader.dataCount(data);
+        for(Double key: courses.keySet()){
+            System.out.println(key + " : " + courses.get(key));
+        }
+    }
 }
